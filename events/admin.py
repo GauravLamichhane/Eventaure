b6496286cfc32dda4a811b85f5b9493bf323d79d
Gaurav <gaurav.lamichhane02@gmail.com>
2026-06-15 00:03:01 +0545

    feat: add registrations count in Registration (admin panel)

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Event, Registration
 
 
@@ -14,6 +15,15 @@ class EventAdmin(admin.ModelAdmin):
   search_fields = ["title", "organizer"]
 
 
+
 @admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
-  list_display = ["event","attendee"]
+    list_display = ["event", "attendee_count"]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            attendee_count_value=Count("event__event_registrations")
+        )
+
+    def attendee_count(self, obj):
+        return obj.attendee_count_value
