7a29300b3f831d92c3320e045ffc9e080ec2efa4
Gaurav <gaurav.lamichhane02@gmail.com>
2026-06-28 22:36:32 +0545

    fix: serve static file in dev server

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -7,4 +9,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("users.urls")),
     path("api/", include("events.urls")),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
