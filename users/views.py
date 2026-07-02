bafde78d033612636e50f0de8f6993dfa11dacf6
Gaurav <gaurav.lamichhane02@gmail.com>
2026-07-02 20:27:07 +0545

    refactor: add id field to the profileview

--- users/views.py
+++ users/views.py
@@ -18,6 +18,7 @@ class ProfileView(APIView):
     def get(self, request):
         return Response(
             {
+                "id": request.user.id,
                 "email": request.user.email,
                 "first_name": request.user.first_name,
                 "last_name": request.user.last_name,
